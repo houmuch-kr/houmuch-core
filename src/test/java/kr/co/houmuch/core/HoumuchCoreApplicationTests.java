3f3366faa9595d2ccd03cffec8f25f7044cca98a
chaehun <chaehun@uniess.co.kr>
2022-12-28 12:44:57 +0900

    최초 커밋

---
+++ src/test/java/kr/co/houmuch/core/HoumuchCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.houmuch.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoumuchCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
