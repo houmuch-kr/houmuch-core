58afc4aca87bd3fee20c1bb89c83734ba54a3d26
chaehun <chaehun@uniess.co.kr>
2023-01-10 11:40:44 +0900

    feat : AreaCodeJpaRepository default method 작성

--- src/main/java/kr/co/houmuch/core/domain/code/AreaCodeJpaRepository.java
+++ src/main/java/kr/co/houmuch/core/domain/code/AreaCodeJpaRepository.java
@@ -8,25 +8,38 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AreaCodeJpaRepository extends JpaRepository<AreaCodeJpo, Long> {
     Page<AreaCodeJpo> findByType(int type, Pageable pageable);
+    Page<AreaCodeJpo> findByCodeSidoAndCodeSggAndCodeUmd(Integer sido, Integer sgg, Integer umd, Pageable pageable);
+    
+    default Page<AreaCodeJpo> findBySido(int sido, Pageable pageable) {
+        return findByCodeSidoAndCodeSggAndCodeUmd(sido, null, null, pageable);
+    }
+
+    default Page<AreaCodeJpo> findBySidoAndSgg(int sido, int sgg, Pageable pageable) {
+        return findByCodeSidoAndCodeSggAndCodeUmd(sido, sgg, null, pageable);
+    }
+
+    default Page<AreaCodeJpo> findBySidoAndSggAndUmd(int sido, int sgg, int umd, Pageable pageable) {
+        return findByCodeSidoAndCodeSggAndCodeUmd(sido, sgg, umd, pageable);
+    }
 
     /**
      * 광역시/도 타입 지역 목록 조회 (type : 0)
      */
-    default Page<AreaCodeJpo> findSido(Pageable pageable) {
+    default Page<AreaCodeJpo> findSidoAll(Pageable pageable) {
         return findByType(0, pageable);
     }
 
     /**
      * 시/군/구 타입 지역 목록 조회 (type : 1)
      */
-    default Page<AreaCodeJpo> findSgg(Pageable pageable) {
+    default Page<AreaCodeJpo> findSggAll(Pageable pageable) {
         return findByType(1, pageable);
     }
 
     /**
      * 읍/면/동 타입 지역 목록 조회 (type : 2)
      */
-    default Page<AreaCodeJpo> findUmd(Pageable pageable) {
+    default Page<AreaCodeJpo> findUmdAll(Pageable pageable) {
         return findByType(2, pageable);
     }
 }
