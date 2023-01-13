ed74aa8f090a50198b8dfa138f4d09282da463d3
chaehun <chaehun@uniess.co.kr>
2023-01-13 15:20:28 +0900

    feat : CombinedAreaCode 10자리 코드 Getter 메소드 작성

--- src/main/java/kr/co/houmuch/core/domain/common/dto/CombinedAreaCode.java
+++ src/main/java/kr/co/houmuch/core/domain/common/dto/CombinedAreaCode.java
@@ -13,6 +13,23 @@ public class CombinedAreaCode {
     private Integer sgg;
     private Integer umd;
 
+    public Long getFullCode() {
+        long code = 0;
+        if (sido != 0) {
+            code = sido * 100000000;
+        }
+        if (sgg != 0) {
+            code += sgg * 100000;
+        }
+        if (umd != 0) {
+            code += umd;
+        }
+        if (code < 1100000000) {
+            throw new IllegalArgumentException("법정동 표준코드는 1100000000 미만일 수 없습니다.");
+        }
+        return code;
+    }
+
     public static CombinedAreaCode of(Integer sido) {
         return of(sido, null, null);
     }
