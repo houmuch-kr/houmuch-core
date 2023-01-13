e17575943474ab11ae333ce1b4f6434599758489
chaehun <chaehun@uniess.co.kr>
2023-01-13 14:46:07 +0900

    feat : @SpringBootApplication Main 클래스 추가
    - 지우고난 후 SpringBoot Test Auto Configuration이 되지 않음

---
+++ src/main/java/kr/co/houmuch/core/HoumuchCoreApplication.java
@@ -0,0 +1,11 @@
+package kr.co.houmuch.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoumuchCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HoumuchCoreApplication.class, args);
+    }
+}
