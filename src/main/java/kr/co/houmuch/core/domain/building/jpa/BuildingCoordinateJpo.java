eef0d6712d89c34b6ad33e54424cf5566232b145
chaehun <chaehun@uniess.co.kr>
2023-01-21 13:57:57 +0900

    feat : BuildingCoordinate 엔티티 작성
    - 건물 좌표 정보

---
+++ src/main/java/kr/co/houmuch/core/domain/building/jpa/BuildingCoordinateJpo.java
@@ -0,0 +1,35 @@
+package kr.co.houmuch.core.domain.building.jpa;
+
+import kr.co.houmuch.core.domain.common.jpa.CombinedAreaCodeJpo;
+import lombok.*;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "building_coordinate")
+@EntityListeners(AuditingEntityListener.class)
+public class BuildingCoordinateJpo {
+    @Id
+    @Column(name = "building_id")
+    private String id;
+
+    @Embedded
+    private CombinedAreaCodeJpo code;
+
+    @MapsId("id")
+    @ToString.Exclude
+    @OneToOne(mappedBy = "coordinate")
+    @JoinColumn(name = "id")
+    private BuildingJpo building;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
