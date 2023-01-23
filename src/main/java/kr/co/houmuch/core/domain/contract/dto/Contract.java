a523b2499cb6a361c4cb3c78498fa324441121db
hyodii <hyodii9736@gmail.com>
2023-01-23 16:24:21 +0900

    feat : Contract DTO of 메소드 추가

--- src/main/java/kr/co/houmuch/core/domain/contract/dto/Contract.java
+++ src/main/java/kr/co/houmuch/core/domain/contract/dto/Contract.java
@@ -1,5 +1,6 @@
 package kr.co.houmuch.core.domain.contract.dto;
 
+import kr.co.houmuch.core.domain.common.dto.CombinedAreaCode;
 import kr.co.houmuch.core.domain.contract.BuildingType;
 import kr.co.houmuch.core.domain.contract.ContractType;
 import lombok.*;
@@ -20,4 +21,9 @@ public class Contract {
     private LocalDate contractedAt;
     private String serialNumber;
     private String name;
+
+    public static Contract of(String id, ContractType type, BuildingType buildingType, Long areaCode, LocalDate contractedAt, String serialNumber, String name) {
+        return new Contract(id, type, buildingType, areaCode, contractedAt, serialNumber, name);
+    }
+
 }
