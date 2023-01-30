a1059f2e34ca04ca2d1cf9ee176eace770fdc620
YangGeumhee <rmagml9769@gmail.com>
2023-01-30 13:23:30 +0900

    feat : areaCode, contract DTO entityOf 메소드 작성

--- src/main/java/kr/co/houmuch/core/domain/building/dto/Building.java
+++ src/main/java/kr/co/houmuch/core/domain/building/dto/Building.java
@@ -1,6 +1,7 @@
 package kr.co.houmuch.core.domain.building.dto;
 
 import kr.co.houmuch.core.domain.code.dto.AreaCode;
+import kr.co.houmuch.core.domain.common.dto.Coordinate;
 import kr.co.houmuch.core.domain.contract.BuildingType;
 import lombok.*;
 
@@ -13,9 +14,10 @@ import lombok.*;
 public class Building {
     private String id;
     private String name;
-    private AreaCode areaCode;
+    private Long areaCode;
     private BuildingType type;
-    private Double addressDetail;
+    private String addressDetail;
     private Double squareMeter;
     private Integer builtAt;
+    private Coordinate coordinate;
 }
--- src/main/java/kr/co/houmuch/core/domain/contract/dto/Contract.java
+++ src/main/java/kr/co/houmuch/core/domain/contract/dto/Contract.java
@@ -1,8 +1,11 @@
 package kr.co.houmuch.core.domain.contract.dto;
 
+import kr.co.houmuch.core.domain.building.dto.Building;
 import kr.co.houmuch.core.domain.common.dto.CombinedAreaCode;
+import kr.co.houmuch.core.domain.common.dto.Coordinate;
 import kr.co.houmuch.core.domain.contract.BuildingType;
 import kr.co.houmuch.core.domain.contract.ContractType;
+import kr.co.houmuch.core.domain.contract.jpa.ContractJpo;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -16,9 +19,29 @@ import java.time.LocalDate;
 public class Contract {
     private String id;
     private ContractType type;
-    private BuildingType buildingType;
-    private Long areaCode;
     private LocalDate contractedAt;
     private String serialNumber;
-    private String name;
+    private Building building;
+
+    public static Contract entityOf(ContractJpo contractJpo) {
+        return builder()
+                .id(contractJpo.getId())
+                .type(contractJpo.getType())
+                .contractedAt(contractJpo.getContractedAt())
+                .serialNumber(contractJpo.getSerialNumber())
+                .building(Building.builder()
+                        .id(contractJpo.getBuilding().getId())
+                        .name(contractJpo.getBuilding().getName())
+                        .areaCode(contractJpo.getBuilding().getAreaCode().getId())
+                        .type(contractJpo.getBuilding().getType())
+                        .addressDetail(contractJpo.getBuilding().getAddressDetail())
+                        .squareMeter(contractJpo.getBuilding().getSquareMeter())
+                        .builtAt(contractJpo.getBuilding().getBuiltAt())
+                        .coordinate(Coordinate.of(
+                                contractJpo.getBuilding().getCoordinate().getCoordinate().getLatitude(),
+                                contractJpo.getBuilding().getCoordinate().getCoordinate().getLongitude()))
+                        .build()
+                )
+                .build();
+    }
 }
