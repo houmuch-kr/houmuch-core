41b84b5cfc64c82f05f97f58f626936fffa7a58c
hyodii <hyodii9736@gmail.com>
2023-02-04 15:30:15 +0900

    fix : building dto 타입 변경

--- src/main/java/kr/co/houmuch/core/domain/building/dto/Building.java
+++ src/main/java/kr/co/houmuch/core/domain/building/dto/Building.java
@@ -1,7 +1,6 @@
 package kr.co.houmuch.core.domain.building.dto;
 
 import kr.co.houmuch.core.domain.code.dto.AreaCode;
-import kr.co.houmuch.core.domain.common.dto.Coordinate;
 import kr.co.houmuch.core.domain.contract.BuildingType;
 import lombok.*;
 
@@ -14,10 +13,9 @@ import lombok.*;
 public class Building {
     private String id;
     private String name;
-    private Long areaCode;
+    private AreaCode areaCode;
     private BuildingType type;
-    private String addressDetail;
+    private Double addressDetail;
     private Double squareMeter;
     private Integer builtAt;
-    private Coordinate coordinate;
 }
