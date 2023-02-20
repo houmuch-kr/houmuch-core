7384798262f29121b36d54dda124eb4e381e8850
chaehun <chaehun@uniess.co.kr>
2023-02-20 13:45:50 +0900

    feat : 실거래 DTO 필드 추가 - floor, price, monthlyPrice

--- src/main/java/kr/co/houmuch/core/domain/contract/dto/Contract.java
+++ src/main/java/kr/co/houmuch/core/domain/contract/dto/Contract.java
@@ -1,5 +1,6 @@
 package kr.co.houmuch.core.domain.contract.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import kr.co.houmuch.core.domain.building.dto.Building;
 import kr.co.houmuch.core.domain.contract.ContractType;
 import kr.co.houmuch.core.domain.contract.jpa.ContractJpo;
@@ -13,11 +14,15 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Contract {
     private String id;
     private ContractType type;
     private LocalDate contractedAt;
     private String serialNumber;
+    private Integer floor;
+    private int price;
+    private Integer monthlyPrice;
     private Building building;
 
     public static Contract entityOf(ContractJpo contractJpo) {
@@ -26,6 +31,9 @@ public class Contract {
                 .type(contractJpo.getType())
                 .contractedAt(contractJpo.getContractedAt())
                 .serialNumber(contractJpo.getSerialNumber())
+                .floor(contractJpo.getDetail().getFloor())
+                .price(contractJpo.getDetail().getPrice())
+                .monthlyPrice(contractJpo.getDetail().getMonthlyPrice())
                 .building(Building.entityOf(contractJpo.getBuilding()))
                 .build();
     }
