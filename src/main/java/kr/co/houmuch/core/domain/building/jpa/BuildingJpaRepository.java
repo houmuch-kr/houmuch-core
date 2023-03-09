0ba58e38b8c40e3f73c1ff9cc4ee9c03fe62f6c1
hyodii <hyodii9736@gmail.com>
2023-03-09 14:14:05 +0900

    fix : 검색 JPQL Pageable 추가

--- src/main/java/kr/co/houmuch/core/domain/building/jpa/BuildingJpaRepository.java
+++ src/main/java/kr/co/houmuch/core/domain/building/jpa/BuildingJpaRepository.java
@@ -3,6 +3,7 @@ package kr.co.houmuch.core.domain.building.jpa;
 import kr.co.houmuch.core.domain.code.AreaCodeJpo;
 import kr.co.houmuch.core.domain.contract.BuildingType;
 import kr.co.houmuch.core.domain.contract.jpa.ContractJpo;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,8 +17,6 @@ public interface BuildingJpaRepository extends JpaRepository<BuildingJpo, String
     @Query("SELECT c FROM ContractJpo c " +
             "INNER JOIN FETCH c.building cb " +
             "INNER JOIN FETCH c.additional ca " +
-            // 나중에 coordinate 데이터 다 넣으면 아래 주석 사용해야 함!
-//            "LEFT OUTER JOIN FETCH c.coordinate cc " +
             "INNER JOIN FETCH cb.coordinate bc " +
             "INNER JOIN FETCH c.detail cd " +
             "INNER JOIN FETCH cb.areaCode a " +
@@ -31,6 +30,6 @@ public interface BuildingJpaRepository extends JpaRepository<BuildingJpo, String
     @Query("SELECT b FROM BuildingJpo b " +
             "INNER JOIN FETCH b.coordinate bc " +
             "WHERE b.name like %:searchWord%")
-    List<BuildingJpo> findByName(String searchWord);
+    List<BuildingJpo> findByName(String searchWord, Pageable pageable);
 }
 
